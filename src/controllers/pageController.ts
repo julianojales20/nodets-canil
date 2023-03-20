6bea781a1ebba438bc7afafcb1cca66ae1b006b0
Juliano Alonso <julianojales20@gmail.com>
2023-03-20 16:11:20 -0300

    final project 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,25 +1,62 @@
 import { Request, Response } from "express";
 
-export const home = (req: Request, res: Response) => {
-    res.send('home no controller');
-    // res.render('pages/page');
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
 
+export const home = (req: Request, res: Response) => {
+    let list = Pet.getAll();
+
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os Animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    });
 }
 
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');
 
-    // res.render('pages/dogs');
-
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    
+    });
 }
 
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');
 
-    // res.render('pages/cats');
-
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list 
+    
+    });
 }
 
-export const fisches = (req: Request, res: Response) => {
+export const fish = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');
 
-    // res.render('pages/fisches');
-
+    res.render('pages/page', { 
+        menu: createMenuObject('fish'),
+        banner: {
+           
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    
+    });
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', PageController.home);
 router.get('/dogs', PageController.dogs);
 router.get('/cats', PageController.cats);
-router.get('/fisches', PageController.fisches);
+router.get('/fish', PageController.fish);
 
 router.get('/search', SearchController.search);
 
